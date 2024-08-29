bd6f9d4229940b99433c83ad504b9a0b37752a37
Vict-gtz <w8767g@gmail.com>
2024-08-29 19:37:09 -0400

    paciente_estructura.cpp realizado

--- paciente_estructura.cpp
+++ paciente_estructura.cpp
@@ -1,2 +1,25 @@
-sda
+#include <iostream>
 
+// Estructura definida llamada "Persona"
+struct Persona {
+    std::string nombre;
+    int edad;
+    double altura;
+};
+
+int main() {
+    //Instancia para que persona tome valores
+    Persona persona;
+
+    // Agregar información a persona
+    persona.nombre = "Juan";
+    persona.edad = 20;
+    persona.altura = 2.40;
+
+    // acceso a la información y print
+    std::cout << "// Nombre: " << persona.nombre << std::endl;
+    std::cout << "// Edad: " << persona.edad << " años" << std::endl;
+    std::cout << "// Altura: " << persona.altura << "m." << std::endl;
+    
+    return 0;
+}
