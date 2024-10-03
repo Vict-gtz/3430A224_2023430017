ba8e7ca9116619a7f2b29209d36674b9b3628ed7
Vict-gtz <w8767g@gmail.com>
2024-10-03 22:21:10 +0000

    se integra un menú interactivo

--- paciente_dlist_estudios_pda.cpp
+++ paciente_dlist_estudios_pda.cpp
@@ -71,7 +71,53 @@ void read_file() {
     csvFile.close();
 }
 
+void menu() {
+    int respuesta = 0;
+
+    while (true) {
+        cout << "\n\n\n------------[Menú]------------" << endl;
+        cout << "1 -> Agregar un Paciente." << endl;
+        cout << "2 -> Organizar prioridad." << endl;
+        cout << "3 -> Buscar por IMC." << endl;
+        cout << "4 -> Buscar por A1C." << endl;//eliminar o editar por los buscar
+        cout << "5 -> Eliminar todos los datos." << endl;
+        cout << "6 -> Mostrar datos." << endl;
+        cout << "7 -> Salir." << endl;
+
+        // Devolverse si está mala la respuesta
+        cin >> respuesta;
+        if (respuesta < 1 || respuesta > 6) {
+            cout << "[Seleccione una opción entre las disponibles (escribir número)]" << endl;
+            continue; 
+        }
+
+
+        //caso de respuesta correcta
+        switch (respuesta) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5://reset datos
+                create_newfile();
+                cout << "Se han reseteado los datos." << endl;
+                break;
+            case 6://mostrar lista
+                read_file();
+                break;
+            case 7://fin programa
+                return;
+        }
+    }
+}
+
 int main() {
-    read_file(); // Lee el archivo y procesa los datos directamente
+    menu(); //multiples opciones
+
+    cout << "El programa ha finalizado" << endl;
     return 0;
 }
