07f1b7392d84d5e38abce28b137bafba608ab092
Vict-gtz <w8767g@gmail.com>
2024-10-04 00:06:20 +0000

    menú opciones de busqueda avance

--- paciente_dlist_estudios_pda.cpp
+++ paciente_dlist_estudios_pda.cpp
@@ -71,6 +71,171 @@ void read_file() {
     csvFile.close();
 }
 
+// buscar el paciente más cercano a A1C ingresado
+pacientes buscar_paciente_por_imc(double imc_usuario) {
+    //abrir archivo
+    std::ifstream csvFile("pacientes.csv"); 
+
+    std::string line;
+    getline(csvFile, line);
+
+
+    pacientes paciente_cercano;
+    double diferencia_minima = 1000; // Inicia con un número grande para comparar
+
+    while (getline(csvFile, line)) {
+        stringstream ss(line);
+        pacientes paciente;
+        string field;
+
+        getline(ss, field, ',');
+        paciente.numero_lista = stoi(field); // Nº
+
+        getline(ss, field, ',');
+        paciente.prioridad = stoi(field); // Prioridad
+
+        getline(ss, paciente.nombre, ','); // Nombre
+
+        getline(ss, paciente.apellido, ','); // Apellido
+
+        getline(ss, field, ',');
+        paciente.edad = stoi(field); // Edad
+
+        getline(ss, field, ',');
+        paciente.imc = stod(field); // IMC
+
+        getline(ss, field, ',');
+        paciente.a1c = stod(field); // A1C
+
+        // Calcular la diferencia entre el IMC ingresado y el IMC del paciente
+        double diferencia = std::abs(paciente.imc - imc_usuario);
+
+        // Actualizar el paciente más cercano si la diferencia es menor
+        if (diferencia < diferencia_minima) {
+            diferencia_minima = diferencia;
+            paciente_cercano = paciente;
+        }
+    }
+
+    csvFile.close();
+    return paciente_cercano; // Retornar el paciente más cercano encontrado
+}
+
+// buscar el paciente más cercano a A1C ingresado
+pacientes buscar_paciente_por_a1c(double a1c_usuario) {
+    //abrir archivo
+    std::ifstream csvFile("pacientes.csv"); 
+
+    std::string line;
+    getline(csvFile, line);
+
+
+    pacientes paciente_cercano;
+    double diferencia_minima = 20; // Inicia con un número grande para comparar
+
+    while (getline(csvFile, line)) {
+        stringstream ss(line);
+        pacientes paciente;
+        string field;
+
+        getline(ss, field, ',');
+        paciente.numero_lista = stoi(field); // Nº
+
+        getline(ss, field, ',');
+        paciente.prioridad = stoi(field); // Prioridad
+
+        getline(ss, paciente.nombre, ','); // Nombre
+
+        getline(ss, paciente.apellido, ','); // Apellido
+
+        getline(ss, field, ',');
+        paciente.edad = stoi(field); // Edad
+
+        getline(ss, field, ',');
+        paciente.imc = stod(field); // IMC
+
+        getline(ss, field, ',');
+        paciente.a1c = stod(field); // A1C
+
+        // Calcular la diferencia entre el A1C ingresado y el A1C del paciente
+        double diferencia = std::abs(paciente.a1c - a1c_usuario);
+
+        // Actualizar el paciente más cercano si la diferencia es menor
+        if (diferencia < diferencia_minima) {
+            diferencia_minima = diferencia;
+            paciente_cercano = paciente;
+        }
+    }
+
+    csvFile.close();
+    return paciente_cercano; // Retornar el paciente más cercano encontrado
+}
+
+
+int caso_cuatro() {
+    double a1c_user; // Permite decimales
+    cout << "Ingrese un índice de A1C [0 ≤ x ≤ 12]: ";
+
+    while (true) {
+        cin >> a1c_user;
+
+        if (cin.fail() || a1c_user < 0 || a1c_user > 12) { // Valores erroneos
+            caso_cuatro();
+        } else { // Valores apropiados
+            break; 
+        }
+    }
+
+    // Buscar paciente cercano al A1C ingresado por el usuario
+    pacientes paciente_encontrado = buscar_paciente_por_a1c(a1c_user);
+
+    // Mostrar los datos del paciente encontrado
+    if (paciente_encontrado.numero_lista != 0) {
+        cout << "\n\n\n------------[Se ha encontrado al paciente:]------------" << endl;
+        cout << "Nº: " << paciente_encontrado.numero_lista 
+        << ", Prioridad: " << paciente_encontrado.prioridad
+        << ", Nombre: " << paciente_encontrado.nombre 
+        << ", Apellido: " << paciente_encontrado.apellido 
+        << ", Edad: " << paciente_encontrado.edad 
+        << ", IMC: " << paciente_encontrado.imc 
+        << ", A1C: " << paciente_encontrado.a1c << endl;
+        //lógica editar/borrar/editar paciente
+        return paciente_encontrado.numero_lista;
+    } else {
+        cout << "No se encontraron pacientes en el archivo." << endl;
+        return 0;
+    }
+}
+
+int caso_tres() {
+    int imc_user; 
+    cout << "Ingrese un índice de IMC: ";
+    cin >> imc_user;
+    // Buscar paciente cercano al IMC ingresado por el usuario
+    pacientes paciente_encontrado = buscar_paciente_por_imc(imc_user);
+
+    // Mostrar los datos del paciente encontrado
+    if (paciente_encontrado.numero_lista != 0) {
+        cout << "\n\n\n------------[Se ha encontrado al paciente:]------------" << endl;
+        cout << "Nº: " << paciente_encontrado.numero_lista 
+        << ", Prioridad: " << paciente_encontrado.prioridad
+        << ", Nombre: " << paciente_encontrado.nombre 
+        << ", Apellido: " << paciente_encontrado.apellido 
+        << ", Edad: " << paciente_encontrado.edad 
+        << ", IMC: " << paciente_encontrado.imc 
+        << ", A1C: " << paciente_encontrado.a1c << endl;
+        //lógica editar/borrar/editar paciente
+        return paciente_encontrado.numero_lista;
+    } else {
+        cout << "No se encontraron pacientes en el archivo." << endl;
+        return 0;
+    }
+}
+
+void editing(int num_paciente) {
+    cout << "hola" << endl;
+}
+
 void menu() {
     int respuesta = 0;
 
@@ -86,7 +251,7 @@ void menu() {
 
         // Devolverse si está mala la respuesta
         cin >> respuesta;
-        if (respuesta < 1 || respuesta > 6) {
+        if (respuesta < 1 || respuesta > 7) {
             cout << "[Seleccione una opción entre las disponibles (escribir número)]" << endl;
             continue; 
         }
@@ -98,17 +263,25 @@ void menu() {
                 break;
             case 2:
                 break;
-            case 3:
+            case 3: {//busqueda IMC
+                int num_paciente = caso_tres();
+                editing(num_paciente);
                 break;
-            case 4:
+            }
+            case 4: {//busqueda a1c
+                int num_paciente = caso_cuatro();
+                editing(num_paciente);
                 break;
-            case 5://reset datos
+            }
+            case 5: {//reset datos
                 create_newfile();
                 cout << "Se han reseteado los datos." << endl;
                 break;
-            case 6://mostrar lista
+            }
+            case 6: {//mostrar lista
                 read_file();
                 break;
+            }
             case 7://fin programa
                 return;
         }
