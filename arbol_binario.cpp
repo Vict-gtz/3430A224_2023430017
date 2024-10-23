237d07431236c8cdd212b07d29eedaa813a43e4e
Vict-gtz <w8767g@gmail.com>
2024-10-23 18:31:15 -0300

    reemplazo y eliminación de nodo implementado

--- arbol_binario.cpp
+++ arbol_binario.cpp
@@ -75,8 +75,76 @@ private:
         cout << node->info << " ";
     }
 
+    // Busca si un valor esta en el arbol o no
+    bool search(Node* node, int data) const {
+        if (node == nullptr) {
+            return false;  // El valor no esta en el arbol
+        }
+
+        if (node->info == data) {
+            return true;  // El valor si esta en el arbol
+        }
+
+        if (data < node->info) {
+            return search(node->left, data);  // Buscar en el subarbol izquierdo
+        } else {
+            return search(node->right, data);  // Buscar en el subarbol derecho
+        }
+    }
+
+    Node* deleteNode(Node* root, int data) {
+        if (root == nullptr) {
+            return root;  // Base case: if the tree is empty
+        }
+
+        // Recorrer el arbol para encontrar el nodo que se desea eliminar
+        if (data < root->info) {
+            root->left = deleteNode(root->left, data);  // Buscar en el subarbol izquierdo
+        } else if (data > root->info) {
+            root->right = deleteNode(root->right, data);  // Buscar en el subarbol derecho
+        } else {
+            // Caso 1: Nodo con un solo hijo o sin hijos
+            if (root->left == nullptr) {
+                Node* temp = root->right;
+                delete root;
+                return temp;
+            } else if (root->right == nullptr) {
+                Node* temp = root->left;
+                delete root;
+                return temp;
+            }
+
+            // Caso 2: Nodo con dos hijos
+            // Buscar el sucesor inorden (el nodo más pequeño en el subarbol derecho)
+            Node* temp = minValueNode(root->right);
+
+            // Copiar el valor del sucesor inorden al nodo actual
+            root->info = temp->info;
+
+            // Eliminar el sucesor inorden
+            root->right = deleteNode(root->right, temp->info);
+        }
+
+        return root;
+    }
+
+    // Encontrar el valor mínimo en un subárbol
+    Node* minValueNode(Node* node) {
+        Node* current = node;
+        while (current && current->left != nullptr) {
+            current = current->left;
+        }
+        return current;
+    }
+
+
 public:
     Arbol() : root(nullptr) {}
+
+    // Metodo publico para borrar un nodo en el arbol
+    void deleteValue(int data) {
+        root = deleteNode(root, data);
+    }
 
     // Metodo publico para insertar un nodo en el arbol
     void insert(int data) {
@@ -99,6 +167,9 @@ public:
         cout << endl;
     }
 
+    bool search(int data) const {
+        return search(root, data);
+    }
 };
 
 int main() {
@@ -164,12 +235,49 @@ int main() {
             }
             case 4: {
                 int trabajando = 0;
-                arbol.printInOrder();
+                int opcionn = 0;
+                arbol.printInOrder(); // Muestra el árbol al usuario
 
-                // Pregunta valor que quiere utilizar
+                // Pregunta el valor que quiere utilizar
                 cout << "++ Con que valor quieres trabajar? :";
                 while (true) {
-                    if (cin >> trabajando) { // Valor correcto ingresado
+                    if (cin >> trabajando) {
+                        // Verifica si el valor existe en el árbol
+                        if (arbol.search(trabajando)) {
+                            break;  // Valor correcto: continua
+                        } else {
+                            cout << "XX El valor no está en el árbol, intente otra vez XX: ";
+                        }
+                    } else {  // Valor incorrecto: limpia buffer y vuelve a pedir
+                        cin.clear();
+                        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                        cout << "xx Ingrese un valor numérico válido xx: ";
+                    }
+                }
+
+                // Pregunta qué quiere hacer con este valor
+                cout << "\n\n++ Que quieres hacer con este valor?\n1 -> Eliminarlo <-\n2 -> Reemplazarlo <-" << endl;
+                while (!(opcionn == 1 || opcionn ==2)) {
+                    cin.clear();
+                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                    cout << "[1 o 2]: ";
+                    cin >> opcionn;
+                }
+                if (opcionn == 1) {
+                    arbol.deleteValue(trabajando);  // Llamar a la función pública para eliminar el nodo
+                    cout << "-> Nodo eliminado con éxito <-" << endl; // Y agrega un aviso de que se elimino
+                    break; 
+                } else {
+                    arbol.deleteValue(trabajando); 
+                    //acá no agregué el break; para que pase directo a preguntar el valor que se va a agregar en el case 5
+                }
+            }
+            case 5: { // Agregar valores al árbol
+                int valor_nuevo = 0;
+                cout << "\n++ Valor nuevo a agregar al arbol: ";
+
+                while (true) {
+                    if (cin >> valor_nuevo) { // Valor correcto ingresado
                         break;
                     } else { // Valor incorrecto: limpia buffer y pregunta otra vez
                         cin.clear();
@@ -178,30 +286,9 @@ int main() {
                     }
                 }
 
-                // Pregunta qué quiere hacer con este valor
-                cout << "++ Que quieres hacer con este valor?\n1 -> Eliminarlo <-\n2 -> Reemplazarlo <-" << endl;
-                while (true) {
-                    
-                }
+                arbol.insert(valor_nuevo); // Agrega el valor al arbol
                 break;
             }
-            case 5: { // Agregar valores al árbol
-            int valor_nuevo = 0;
-            cout << "\n++ Valor nuevo a agregar al arbol: ";
-
-            while (true) {
-                if (cin >> valor_nuevo) { // Valor correcto ingresado
-                    break;
-                } else { // Valor incorrecto: limpia buffer y pregunta otra vez
-                    cin.clear();
-                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-                    cout << "xx Ingrese un valor numérico válido xx: ";
-                }
-            }
-
-            arbol.insert(valor_nuevo); // Agrega el valor al arbol
-            break;
-        }
             case 6: {
                 cout << "Programa finalizado" << endl; // Finaliza programa
                 return 0;
